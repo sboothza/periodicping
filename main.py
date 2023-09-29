e5422b5ad531b4eb04860af6afb74c3eb870fb06
sgb-spin <144226803+sgb-spin@users.noreply.github.com>
2023-09-29 11:59:57 +0200

    SB: Added some documentation and dnslookup

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+import argparse
 import datetime, threading, time
 from pythonping import ping
 import mysql.connector
 
+import dns
+import dns.resolver
+
 next_call = time.time()
 
+dbuser = "none"
+dbpass = "none"
+dbhost = "none"
+dbname = "none"
 
-def insert_row(ping_time, packet_loss, average_time, max_time, min_time):
+
+def insert_row(ping_time, packet_loss, average_time, max_time, min_time, dns_ip, dns_time):
     connection = None
     try:
-        connection = mysql.connector.connect(user="root", password="or9asm1c", host="localhost", database="trace")
+        connection = mysql.connector.connect(user=dbuser, password=dbpass, host=dbhost, database=dbname)
         cursor = connection.cursor(buffered=True)
         params = {"pingtime": ping_time, "packet_loss": packet_loss, "average_time": average_time, "max_time": max_time,
-                  "min_time": min_time}
-        cursor.execute("insert into ping (pingtime, packet_loss, average_time, max_time, min_time) values "
-                       "(%(pingtime)s, %(packet_loss)s, %(average_time)s, %(max_time)s, %(min_time)s); ", params)
+                  "min_time": min_time, "dns_ip": dns_ip, "dns_time": dns_time}
+        cursor.execute(
+            "insert into ping (pingtime, packet_loss, average_time, max_time, min_time, dns_ip, dns_time) values "
+            "(%(pingtime)s, %(packet_loss)s, %(average_time)s, %(max_time)s, %(min_time)s, %(dns_ip)s, %(dns_time)s); ",
+            params)
         connection.commit()
     except Exception as e:
         print(str(e))
@@ -26,11 +37,20 @@ def main():
     global next_call
     try:
         result = ping("8.8.8.8")
-        # print(str(result))
 
-        print(
-            f"{datetime.datetime.now()}\t{result.packet_loss}\t{result.rtt_avg_ms}\t{result.rtt_max_ms}\t{result.rtt_min_ms}")
-        insert_row(datetime.datetime.now(), result.packet_loss, result.rtt_avg_ms, result.rtt_max_ms, result.rtt_min_ms)
+        time_start = datetime.datetime.now()
+        dns_result = dns.resolver.resolve("www.google.com", 'A')
+        time_dns = datetime.datetime.now() - time_start
+        ip = "not found"
+        try:
+            ip = list(dns_result.response.answer[0].items.keys())[0].address
+        except:
+            pass
+
+        print(f"{datetime.datetime.now()}\t{result.packet_loss}\t{result.rtt_avg_ms}\t{result.rtt_max_ms}\t"
+              f"{result.rtt_min_ms}\t{ip}\t{time_dns.microseconds / 1000}")
+        insert_row(datetime.datetime.now(), result.packet_loss, result.rtt_avg_ms, result.rtt_max_ms, result.rtt_min_ms,
+                   ip, time_dns.microseconds / 1000)
     except Exception as e:
         print(e)
 
@@ -39,5 +59,26 @@ def main():
 
 
 if __name__ == '__main__':
-    print("time\tloss\tavg\tmax\tmin")
+    print("time\tloss\tavg\tmax\tmin\tip\ttdns")
+    parser = argparse.ArgumentParser(description="periodic ping")
+    parser.add_argument('dbhost',
+                        help='DB Host',
+                        type=str)
+    parser.add_argument('dbuser',
+                        help='Db Username',
+                        type=str)
+    parser.add_argument('dbpass',
+                        help='Db Password',
+                        type=str)
+    parser.add_argument('db',
+                        help='Db Name',
+                        type=str)
+
+    args = parser.parse_args()
+
+    dbhost = args.dbhost
+    dbuser = args.dbuser
+    dbpass = args.dbpass
+    dbname = args.db
+
     main()
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Periodic Network Ping Tool
+
+Logs basic network stats to a mysql db  
+Simple enough to convert to any other db if you like  
+Originally logged to tsv file for import, but this became too unstable for log-term use
+
+## Usage
+
+`python main.py <db host> <db user> <db pass> <db name>`
+
+The output to the console is in tsv format - originally to pipe to a file
+
+## DB
+
+```
+CREATE TABLE `ping` (
+  `pingtime` datetime NOT NULL,
+  `packet_loss` float NOT NULL,
+  `average_time` float NOT NULL,
+  `max_time` float NOT NULL,
+  `min_time` float NOT NULL,
+  `dns_ip` varchar(100) DEFAULT NULL,
+  `dns_time` float DEFAULT NULL,
+  PRIMARY KEY (`pingtime`)
+)
+```
+
+### Queries
+
+### 10 Min Stats
+This groups the raw data into 10min buckets
+```
+select concat(concat(date(pingtime), 'H'), concat(lpad(hour(pingtime), 2, '0'), concat(':',lpad(truncate(minute(pingtime), -1), 2, '0')))) as ping_time, 
+max(packet_loss) as packet_loss, avg(average_time) as average_time, max(max_time) as max_time, min(min_time) as min_time, 
+case when max(dns_ip) = 'not found' then 1 else 0 end as dns_fail, max(dns_time) as dns_time
+from ping
+group by ping_time
+```
+
+### Hourly Stats
+This groups the raw data into hourly buckets
+```
+select concat(concat(date(pingtime), 'H'), concat(lpad(hour(pingtime), 2, '0'), ':00')) as ping_time, 
+max(packet_loss) as packet_loss, avg(average_time) as average_time, max(max_time) as max_time, min(min_time) as min_time, 
+case when max(dns_ip) = 'not found' then 1 else 0 end as dns_fail, max(dns_time) as dns_time
+from ping
+group by ping_time
+```
